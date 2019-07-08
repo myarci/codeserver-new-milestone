a8b7c200cd4258108dd666460aa9efc40cfea5af
Metin Yarci <metin.yarci@aurea.com>
2019-07-08 15:25:32 +0300

    project added

---
+++ src/main/java/hello/GreetingService.java
@@ -0,0 +1,42 @@
+package hello;
+
+public class GreetingService {
+
+    private static final String CLASS = "class";
+
+    public void method1(int myInt) {
+
+    }
+
+    public void method1() {
+        System.out.println("1");
+        System.out.println("1");
+        System.out.println("1");
+        System.out.println("1");
+        System.out.println("1");
+        System.out.println("1");
+        System.out.println("1");
+        System.out.println("1");
+        System.out.println("1");
+    }
+
+    public class GreetingService {
+        public void method1(int myInt) {
+
+        }
+
+        public void method1() {
+            System.out.println("1");
+            System.out.println("1");
+            System.out.println("1");
+            System.out.println("1");
+            System.out.println("1");
+            System.out.println("1");
+            System.out.println("1");
+            System.out.println("1");
+            System.out.println("1");
+        }
+    }
+
+}
+
